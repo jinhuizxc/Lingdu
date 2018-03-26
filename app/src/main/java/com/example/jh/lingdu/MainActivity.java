2fb0e3695feb093d3490c49a7d29be14e0da3f98
jinhuizxc <1004260403@qq.com>
2018-03-26 09:05:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/lingdu/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jh.lingdu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 学习吧，骚年
+ * 现在是git用户名/密码要设置一下
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
