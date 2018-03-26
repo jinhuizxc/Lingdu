60e137b17a043d3984dbe2ac3030bf315b855483
jinhuizxc <1004260403@qq.com>
2018-03-26 09:10:43 +0800

    计划从公司这个项目做起，直到完成的时候看能领悟到什么新的东西吧！

--- app/src/main/java/com/example/jh/lingdu/MainActivity.java
+++ app/src/main/java/com/example/jh/lingdu/MainActivity.java
@@ -6,6 +6,14 @@ import android.os.Bundle;
 /**
  * 学习吧，骚年
  * 现在是git用户名/密码要设置一下
+ * 设置本机git 通道cmd：
+ *
+ * git config --global user.email "1004260403@qq.com"
+ * git config --global user.name "jinhuizxc"
+ *
+ * 计划搬到新的住址后开始新的项目的制作，先熟悉项目功能，业务。之后以后怎么说就再看了。
+ *
+ *
  */
 public class MainActivity extends AppCompatActivity {
 
